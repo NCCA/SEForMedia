70a5e2338c568643920b5da8b3dbeb8ec5377ff5
Jon Macey <jmacey@bournemouth.ac.uk>
2024-09-02 19:38:43 +0100

    added operations demo

--- Lecture2/numbers_demo.py
+++ Lecture2/numbers_demo.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
-integer_variable=1  
-float_variable=1.2
-complex_variable=4+5j
+integer_variable = 1
+float_variable = 1.2
+complex_variable = 4 + 5j
 
 print(f"{integer_variable} is of type {type(integer_variable)}")
 print(f"{float_variable} is of type {type(float_variable)}")
 print(f"{complex_variable} is of type {type(complex_variable)}")
-
---
+++ Lecture2/operations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# modify x and y for different numeric types
+x = 2
+y = 5
+print(f"{x+y=}")
+print(f"{x-y=}")
+print(f"{x*y=}")
+print(f"{x/y=}")
+print(f"{x//y=}")
+print(f"{-x=}")
+print(f"{+x=}")
+print(f"{x**y=}")
