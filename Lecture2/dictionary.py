3722a56d227436c4cf89d1f4ab9733300a5d2672
Jon Macey <jmacey@bournemouth.ac.uk>
2024-09-04 09:48:09 +0100

    added all L2 code

---
+++ Lecture2/dictionary.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+colours={
+          'red' : [1,0,0],
+          'green' : [0,1,0],
+          'blue' : [0,0,1],
+          'white' : [1,1,1],
+          'black' : [0,0,0]
+}
+
+print(f"{colours.get('red')=}")
+print(f"{colours.get('green')=}")
+print(f"{colours.get('purple')=}")
+# can also use
+print(f"{colours['white']=}")
+# but
+print(f"{colours['purple']=}")
+
