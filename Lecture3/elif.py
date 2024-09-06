fe074929fd53e14fddedd610e92af09b418c3206
Jon Macey <jmacey@bournemouth.ac.uk>
2024-09-06 10:59:57 +0100

    updated

---
+++ Lecture3/elif.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+try:
+    number = int(input("please enter a number between 1 and 100 : "))
+except ValueError:
+    print("you did not enter a number")
+
+if number < 1 or number > 100:
+    print("the number is not between 1 and 100")
+elif number < 50:
+    print("the number is less than 50")
+else:
+    print("the number is greater than 50")
