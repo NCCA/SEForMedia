270919d7aa1ed241537d00a75e685a0617bd5a1a
Jon Macey <jmacey@bournemouth.ac.uk>
2024-09-19 19:26:20 +0100

    added file demos

---
+++ Seminars/Files/simple_read.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+f = open("test.txt", "r")
+contents = f.read()
+print(contents)
+f.close()
---
+++ Seminars/Files/simple_write.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+f = open("test.txt", "w")
+f.write("Hello, World!")
+f.close()
